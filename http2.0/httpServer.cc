58c9faa90cc3f06c062f68789ea8e1bd9f6defb0
bytenan <bytenan@outlook.com>
2023-05-28 21:12:38 +0800

    解决了response乱码问题，但仍无法解决本地图片无法显示的问题

--- http2.0/httpServer.cc
+++ http2.0/httpServer.cc
@@ -12,6 +12,7 @@ std::string suffixToRescource(const std::string &suffix)
 {
     if (suffix == ".html") return "text/html";
     if (suffix == ".jpg") return "application/x-jpg;image/jpeg";
+    if (suffix == ".png") return "application/x-png;image/png";
     return "text/html";
 }
 
@@ -20,16 +21,16 @@ void Conversion(const Request &request, Response *response)
     response->status = "HTTP/1.1 200 OK\r\n";
     // response->status = "HTTP/1.1 307 Temporary Redirect\r\n";
     response->header = "Content-Type: " + suffixToRescource(request.suffix) + "\r\n";
-    if (request.size > 0)
-        response->header += "Content-Length: " + std::to_string(request.size) + "\r\n";
+    //if (request.size > 0) //这里填充Content-Length是有问题的
+    //    response->header += "Content-Length: " + std::to_string(request.size) + "\r\n";
     // response->header += "Location: https://www.qq.com/\r\n";
     response->blank = "\r\n";
-    char resource[request.size];
-    if (!Util::readFile(request.path, resource, request.size))
+    if (!Util::readFile(request.path, &response->body))
     {
-        Util::readFile(html_404, resource, request.size);
+        Util::readFile(html_404, &response->body);
     }
-    response->body = resource;
+    if (response->body.size() > 0)
+        response->header += "Content-Length: " + std::to_string(response->body.size()) + "\r\n";
 }
 
 int main(int argc, char *argv[])
--- http2.0/util.hpp
+++ http2.0/util.hpp
@@ -6,11 +6,16 @@
 class Util
 {
 public:
-    static bool readFile(const std::string &resource, char *output, int size)
+    static bool readFile(const std::string &resource, std::string *output)
     {
         std::ifstream in(resource, std::ios::binary);
         if (!in.is_open()) return false;
-        in.read(output, size);
+        in.seekg(0, std::ios::end);
+        std::streampos fileSize = in.tellg();
+        in.seekg(0, std::ios::beg);
+        std::vector<char> buffer(fileSize);
+        in.read(buffer.data(), fileSize);
+        (*output) = buffer.data();
         in.close();
         return true;
     }
